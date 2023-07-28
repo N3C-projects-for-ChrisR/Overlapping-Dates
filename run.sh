0712e7571118cc846807b8dec7c3431a7b8853c9
chrisroederucdenver <chris.roeder@cuanschutz.edu>
2023-07-28 15:18:35 -0600

    WIP from past

--- run.sh
+++ run.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env bash
 
-rm -rf  spark-warehouse/learn_spark_db.db/visits
+#rm -rf  spark-warehouse/learn_spark_db.db/visits
 #python3 visits.py
+
+#rm -rf  spark-warehouse/learn_spark_db.db/visits
+#python3 visits_1.py
+
 #python3 visits_2.py
 #python3 visits_2a.py
 #python3 visits_3.py
-python3 visits_3a.py
+#python3 visits_3a.py
+
+
+echo "---- 3b --------------"
+rm -rf  spark-warehouse/learn_spark_db.db/visits
+python3 visits_3b.py
